ab58ece8d2f4bc7fd28204b4dce6f86f950666de
Joan Caballero <70658820+JoanKnight11@users.noreply.github.com>
2021-02-28 17:00:15 +0100

    Done

--- sessio1/s1a.s
+++ sessio1/s1a.s
@@ -1,12 +1,12 @@
 # PRACTICA 1 #######################
 
 	.data
-aa: .byte -5
-bb: .half -344
-cc: .dword -3
-dd: .byte 0xA0
-ee: .word 5799
-ff: .half -1
+aa: 	.byte -5
+bb: 	.half -344
+cc: 	.dword -3
+dd: 	.byte 0xA0
+ee: 	.word 5799
+ff: 	.half -1
 
 	.text
 	.globl main
