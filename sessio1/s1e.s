4709401658f3259408c0079cdbb5f0c0bbdd8e8c
Joan Caballero <70658820+JoanKnight11@users.noreply.github.com>
2021-03-03 18:16:44 +0100

    Done

--- sessio1/s1e.s
+++ sessio1/s1e.s
@@ -1,28 +1,28 @@
 # PRACTICA 1 #######################
 
         .data
-A: 	    .word 3, 5, 7
+A: 	.word 3, 5, 7
 punter: .word 0
 
 	.text
 	.globl main
 main:
-	la $t0, A		      # t0 = &A
-  la $t1, punter		# t1 = &punter
+	la $t0, A		# t0 = &A
+	la $t1, punter		# t1 = &punter
 
-  addiu $t2, $t0, 8	# t2 = &A[2]
-  sw $t2, 0($t1)		# punter = &A[2]
+  	addiu $t2, $t0, 8	# t2 = &A[2]
+  	sw $t2, 0($t1)		# punter = &A[2]
 
-  lw $t3, 0($t2)		# t3 = *punter
-  addiu $s0, $t3, 2	# s0 = *punter + 2;
+ 	 lw $t3, 0($t2)		# t3 = *punter
+ 	 addiu $s0, $t3, 2	# s0 = *punter + 2;
 	
-  lw $t3, -8($t2)		# t3 = *(punter - 2)
-  addu $s0, $t3, $s0	# temp = *(punter - 2) + temp
+ 	 lw $t3, -8($t2)	# t3 = *(punter - 2)
+ 	 addu $s0, $t3, $s0	# temp = *(punter - 2) + temp
 
-  sw $s0, 4($t0)		# A[1] = temp 
+ 	 sw $s0, 4($t0)		# A[1] = temp 
 
-  li $v0, 1
-  addu $a0, $s0, $zero
-  syscall			    # Mostra s0 (= temp) en pantalla
+ 	 li $v0, 1
+ 	 addu $a0, $s0, $zero
+ 	 syscall		# Mostra s0 (= temp) en pantalla
 
-  jr $ra			    # main retorna al codi de startup
+ 	 jr $ra			# main retorna al codi de startup
